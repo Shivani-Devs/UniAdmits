19c1430e8bd99ccb8f7593ced48b9df23ab5b975
Shivani Saravanan <shivanisaravanan2k8@gmail.com>
2026-07-23 00:04:16 -0400

    Configure remote images and fix Vercel build hang
    Added remote image patterns and fixed Vercel build hanging issue.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   images: {
     remotePatterns: [{ protocol: 'https', hostname: '*.supabase.co' }]
+  },
+
+  // Fix Vercel build hanging after successful compilation
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins.push({
+        apply: (compiler) => {
+          compiler.hooks.done.tap('ForceExitPlugin', () => {
+            setTimeout(() => process.exit(0), 0)
+          })
+        }
+      })
+    }
+    return config
   }
 }
 
