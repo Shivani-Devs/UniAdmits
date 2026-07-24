7c22a1ddb794f8c2508a783c49862e139497fb85
Shivani Saravanan <shivanisaravanan2k8@gmail.com>
2026-07-24 12:05:44 -0400

    Remove image patterns and webpack hanging fix
    Removed remote image patterns and webpack configuration to prevent Vercel build hanging.

--- next.config.js
+++ next.config.js
@@ -3,20 +3,6 @@
 const nextConfig = {
   images: {
     remotePatterns: [{ protocol: 'https', hostname: '*.supabase.co' }]
-  },
-
-  // Fix Vercel build hanging after successful compilation
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins.push({
-        apply: (compiler) => {
-          compiler.hooks.done.tap('ForceExitPlugin', () => {
-            setTimeout(() => process.exit(0), 0)
-          })
-        }
-      })
-    }
-    return config
   }
 }
 
